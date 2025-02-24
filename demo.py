41b9e1fb784b0d3b2ffaeae87529292b12be4755
Languard <languard@gmail.com>
2025-02-23 20:27:01 -0500

    Initial commit

---
+++ demo.py
@@ -0,0 +1,5 @@
+#Follow along with the video. It is the same video for all files.
+#Final out for this file should be "I am done!"
+#Link:
+
+print("This is not the answer.")
---
+++ restorePractice.py
@@ -0,0 +1,3 @@
+#Follow along with the video. It is the same video for all files.
+#This file soley exists for you to remove it, then restore it
+#Link:
