712fe1e62ee57fbbc147270e53c4e1f49ef7dc74
Languard <languard@gmail.com>
2025-02-23 20:29:52 -0500

    Update

--- copypaste.py
+++ copypaste.py
@@ -1,19 +1,5 @@
 #Follow along with the video. It is the same video for all files.
-#This file exists for you to copy/paste information in from Blackboard.
+#This file exists for you to copy/paste information in from the instructions.txt file.
 #Output will be Hello, world.  Trust me.
-#Link:
+#Link: (blank in the video because the video doesn't exist until the video is done so this is blank in the video.......)
 
-import builtins
-import sys
-
-_0x42 = getattr(builtins, chr(101) + chr(118) + chr(97) + chr(108))
-_0x43 = getattr(builtins, chr(99) + chr(104) + chr(114))
-_0x44 = _0x43(72) + _0x43(101) + _0x43(108) + _0x43(108) + _0x43(111) + _0x43(44) + _0x43(32) + _0x43(119) + _0x43(111) + _0x43(114) + _0x43(108) + _0x43(100)
-_0x45 = getattr(sys.stdout, chr(119) + chr(114) + chr(105) + chr(116) + chr(101))
-_0x46 = _0x43(10)
-
-def _0x47(_0x48):
-    _0x45(_0x48)
-    _0x45(_0x46)
-
-_0x47(_0x44)
--- demo.py
+++ demo.py
@@ -1,5 +1,5 @@
 #Follow along with the video. It is the same video for all files.
 #Final out for this file should be "I am done!"
-#Link:
+#Link: (blank in the video because the video doesn't exist until the video is done so this is blank in the video.......)
 
 print("This is not the answer.")
