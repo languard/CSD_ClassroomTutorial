2afc8755bf68657fcc36754702399079c8e30d15
Languard <languard@gmail.com>
2025-02-26 12:26:22 -0500

    Updated files

--- copypaste.py
+++ copypaste.py
@@ -2,3 +2,20 @@
 #This file exists for you to copy/paste information in from the instructions.txt file.
 #Output will be Hello, world.  Trust me.
 #Link: https://youtu.be/WLkPJNwg528
+
+
+import builtins
+import sys
+
+_0x42 = getattr(builtins, chr(101) + chr(118) + chr(97) + chr(108))
+_0x43 = getattr(builtins, chr(99) + chr(104) + chr(114))
+_0x44 = _0x43(72) + _0x43(101) + _0x43(108) + _0x43(108) + _0x43(111) + _0x43(44) + _0x43(32) + _0x43(119) + _0x43(111) + _0x43(114) + _0x43(108) + _0x43(100)
+_0x45 = getattr(sys.stdout, chr(119) + chr(114) + chr(105) + chr(116) + chr(101))
+_0x46 = _0x43(10)
+
+
+def _0x47(_0x48):
+    _0x45(_0x48)
+    _0x45(_0x46)
+
+_0x47(_0x44)
--- restorePractice.py
+++ restorePractice.py
@@ -1,3 +1,5 @@
 #Follow along with the video. It is the same video for all files.
 #This file soley exists for you to remove it, then restore it
 #Link: https://youtu.be/WLkPJNwg528
+
+print("meep")
